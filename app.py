81c267a82224e85de857c29e34123607bd8d21e9
GuSoder <GuSoder@gmail.com>
2025-08-20 16:14:26 +0200

    Update app.py with new Flask application structure

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, Flask!"
+def index():
+    return render_template('index.html')
+
+@app.route('/action1')
+def action1():
+    return "You clicked Action 1!"
+
+@app.route('/action2')
+def action2():
+    return "You clicked Action 2!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
